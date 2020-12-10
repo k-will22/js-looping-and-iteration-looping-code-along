b7c8fa1d6894adffa976110b94ce8b634b46963b
k-will22 <thestreetkw@hotmail.com>
2020-12-10 08:09:39 -0500

    commit tree 7d54facb7bdb908cc2a3d36654390b8e823489d7

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+function writeCards(name, surprise) {
+    let arr = []
+    for (let i = 0; i < name.length; i++) {
+        arr.push(`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`)
+    }
+    return arr
+}
+
+let count = 10 
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
